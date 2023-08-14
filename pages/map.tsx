b3d7438f967b41f1a01b45de5340873d5604e5ea
Vadim Tonkikh <70746364+tesheee@users.noreply.github.com>
2023-08-14 17:18:42 +0300

    add map

---
+++ pages/map.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const map = () => {
+  return (
+    <iframe
+      src='http://65.21.196.176:25676/?worldname=world&mapname=surface&zoom=4&x=-203&y=64&z=1816#'
+      style={{ height: "950px" }}
+    ></iframe>
+  );
+};
+
+export default map;
