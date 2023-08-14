42bfd77af8f52fd85a716bfcaf7fde3249ce8157
Vadim Tonkikh <70746364+tesheee@users.noreply.github.com>
2023-08-14 20:09:40 +0300

    Update map.tsx

--- pages/map.tsx
+++ pages/map.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const map = () => {
   return (
     <iframe
-      src='http://65.21.196.176:25676'
+      src='http://65.21.196.176:25676/'
       style={{ height: "950px" }}
     ></iframe>
   );
