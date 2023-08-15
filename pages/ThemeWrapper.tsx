2cf52781906e2b43e02540dca34bf578fc47f8a0
Сру Ли <101261754+maminpos@users.noreply.github.com>
2023-08-15 08:39:16 +0300

    Update ThemeWrapper.tsx

--- pages/ThemeWrapper.tsx
+++ pages/ThemeWrapper.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import "../styles/globals.scss";
 
 const ThemeWrapper = ({ children, isDarkMode }) => {
+  const [isDarkMode, setIsDarkMode] = React.useState(false);
+  
   return (
     <div className={isDarkMode ? 'dark-theme' : 'light-theme'}>
       {children}
