39e155b033d8bc5c41e10b866de89e73eb468c85
Сру Ли <101261754+maminpos@users.noreply.github.com>
2023-08-15 10:28:43 +0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 import MainLayout from "./mainLayout";
 import { Provider } from "react-redux";
-import React from "react";
+import React, { useState } from "react";
 import store from "../redux/store";
 
 function App({ Component, pageProps }: AppProps) {
