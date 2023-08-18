38451fa8535b8edf6d07b14d0bef731fa5aa50fb
Сру Ли <101261754+maminpos@users.noreply.github.com>
2023-08-18 20:33:28 +0300

    Update ProductCard.tsx

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,32 +1,36 @@
 import React from "react";
 import { IPost } from "../pages";
+import ContentLoader from "react-content-loader";
+import ProductSkeleton from "./ModalAuth/ProductSkeleton";
 
 interface IProductCardProps {
   product: IPost;
 }
 
-const ProductCard: React.FC<IProductCardProps> = ({ product }) => {
+const ProductCard: React.FC<IProductCardProps> = ({ product}) => {
   return (
-    <div className='product-card'>
-      <img src={product.images[0]}></img>
-      <div className='product-card-details'>
-        <p style={{ fontSize: "18px" }}>{product.name}</p>
-        <div style={{ display: "flex", alignSelf: "center" }}>
-          <span
-            className='sizes'
-            style={{
-              listStyle: "none",
-              display: "flex",
-              gap: "10px",
-              marginLeft: "5px",
-            }}
-          >
-            {product.description}
-          </span>
-        </div>
-        <p style={{ fontSize: "13px", opacity: "0.5" }}>{product.user.name}</p>
-      </div>
-    </div>
+      <>
+                  <div className='product-card'>
+                      <img src={product.images[0]}></img>
+                      <div className='product-card-details'>
+                          <p style={{ fontSize: "18px" }}>{product.name}</p>
+                          <div style={{ display: "flex", alignSelf: "center" }}>
+                              <span
+                                  className='sizes'
+                                  style={{
+                                      listStyle: "none",
+                                      display: "flex",
+                                      gap: "10px",
+                                      marginLeft: "5px",
+                                  }}>
+                                    {product.description}
+                              </span>
+                          </div>
+                          <p style={{ fontSize: "13px", opacity: "0.5" }}>{product.user.name}</p>
+                      </div>
+                  </div>
+      </>
+
   );
 };
 
