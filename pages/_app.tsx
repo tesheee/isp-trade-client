f269b9fd16ce3eeb20599019e8c84c492abd0c5b
Сру Ли <101261754+maminpos@users.noreply.github.com>
2023-08-18 20:33:08 +0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,10 +8,21 @@ import store from "../redux/store";
 function App({ Component, pageProps }: AppProps) {
   const [isDarkMode, setIsDarkMode] = useState(false);
 
+  React.useEffect(() => {
+      const storedTheme = window.localStorage.getItem('currentTheme');
+      setIsDarkMode(storedTheme === 'true');
+  }, []);
+
+  function OnIsDarkMode() {
+      const newMode = !isDarkMode;
+      window.localStorage.setItem('currentTheme', newMode.toString()); // Convert boolean to string
+      setIsDarkMode(newMode);
+  }
+
   return (
     <Provider store={store}>
       <div className={isDarkMode ? "dark-theme" : "light-theme"}>
-        <MainLayout isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode}>
+        <MainLayout isDarkMode={isDarkMode} setIsDarkMode={OnIsDarkMode}>
           <Component {...pageProps} />
         </MainLayout>
       </div>
