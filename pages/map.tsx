f7c49ad36c3b415898608930d189f38670c24c4f
Сру Ли <101261754+maminpos@users.noreply.github.com>
2023-08-18 07:59:58 +0300

    Update map.tsx

--- pages/map.tsx
+++ pages/map.tsx
@@ -2,10 +2,9 @@ import React from "react";
 
 const map = () => {
   return (
-    <iframe
-      src='http://65.21.196.176:25676/'
-      style={{ height: "950px" }}
-    ></iframe>
+    <a
+      href='http://65.21.196.176:25676/'
+    ></a>
   );
 };
 
